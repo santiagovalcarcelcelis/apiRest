85011c5f0b9c36e93287280e992d0986c6899851
santiago valcarcel <santiagoprueva@gmail.com>
2023-03-28 13:30:18 -0500

    apiRest

---
+++ apiRest-santiago/src/models/server.js
@@ -0,0 +1,39 @@
+const express = require('express')
+// const cors = require('cors');
+// const { dbConection } = require('../database/config');
+class Server{
+    constructor(){
+        this.app = express();
+        this.port= process.env.PORT;
+        
+        this.usuariosPath="/api/usuarios";
+    // conectar a base de datos
+        // this.conectarDB();
+        
+    // Middlewares
+        // this.middlewares();
+    // rutas de mi aplicacion
+        // this.routes();
+    }
+    // async conectarDB(){
+    //     await dbConection();
+    // }
+    // middlewares(){
+    // //cors
+    //     this.app.use(cors());
+    // // lectura y parseo del body
+    //     this.app.use( express.json() );
+    // // directorio publico
+    //     this.app.use(express.static("public"));
+    // }
+    // routes(){
+    //     this.app.use("/api/usuarios", require("../routes/usuarios"))
+    // }
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log(`servidor corriendo en el puerto`,this.port)
+        })
+    }
+}
+
+module.exports = Server;
